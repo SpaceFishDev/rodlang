3aeac4121f14c9a9d1704de993268a0a8bb86e4a
SpaceFishDev <ultragamesalot@gmail.com>
2024-08-01 18:25:55 +0200

    Started parsing AST

--- bin/main.ml
+++ bin/main.ml
@@ -119,10 +119,92 @@ let string_of_token_list ls =
     List.iter (fun tok -> Buffer.add_string buf ((string_of_token tok)^"\n")) ls;
     Buffer.contents buf
 
+type node_type = PROG | FUNCTION | ARGS | TYPENAME | NONE
+type node = {_type: node_type;  _token: token; children: node list}
+
+let string_of_node_type = function
+| PROG -> "Program"
+| FUNCTION -> "Function"
+| ARGS -> "Args"
+| TYPENAME -> "Type Name" 
+| NONE -> "None"
+
+let rec string_of_node_impl n level =
+  let buf = Buffer.create 0 in 
+  
+  if List.length n.children > 0 then(
+    Buffer.add_string buf (indent level (Printf.sprintf "NODE: [%s]=[%s]\n" (string_of_node_type n._type) (string_of_token n._token)));
+    List.iter (fun child -> Buffer.add_string buf (string_of_node_impl child (level+1))) n.children;
+  Buffer.contents buf)
+else
+  indent level (Printf.sprintf "NODE: [%s]=[%s]\n" (string_of_node_type n._type) (string_of_token n._token))
+  and indent x temp = 
+    if x = 0 then
+      temp
+    else 
+      indent (x-1) "    "^temp
+      
+let string_of_node n = 
+  string_of_node_impl n 0
+ 
+let init_node _type _token _children = 
+  {_type = _type; _token = _token; children = _children}
+
+
+let parse_type (tokens : token list) : node * int =
+  match tokens with
+  | head::tail when head._type = KEYWORD ->( match tail with
+    | h::t when h._type = SQROPEN ->(
+        match t with
+        | h::_ when h._type = SQRCLOSE -> (init_node TYPENAME {_type = head._type; value = head.value^"[]"; col = head.col; ln = head.ln;} [],3)
+        | _ -> failwith "Expected ']'"
+    )
+      | _ -> (init_node TYPENAME head [],1)
+  )
+  | h::_ -> (init_node NONE h [],1)
+  | [] -> failwith "Empty"
+
+let rec list_offset ls off = 
+  if off = 0 then
+    ls
+  else
+    match ls with 
+    | _::t -> (list_offset t (off-1))
+    | [] -> ls
+let rec gather_args args tokens = 
+  match tokens with
+  | [] -> args
+  | _::_ -> let (n, off) = (parse_type tokens) in
+  match n._type with 
+  | NONE ->  args
+  | _ -> gather_args (args@[n]) (list_offset tokens off)
+
+let parse_args tokens =
+  match tokens with 
+  | [] -> failwith "Empty"
+  | h::_ -> ([init_node ARGS h (gather_args [] tokens)], 1)
+
+  
+let parse_function tokens = 
+  let name = List.hd tokens in
+  let (args, offset) = parse_args (List.tl tokens) in
+  (init_node FUNCTION name (args), offset) 
+
+let parse tokens = 
+  let p = init_node PROG {_type = NONE; ln = 0; col = 0; value = "Program Token"} [] in 
+  let parse_next tokens = (
+  match tokens with 
+  | h::t when h.value = "def" -> (
+    parse_function t
+  ) 
+  | _ -> failwith "Not Implimented")
+in 
+let (n, _) = parse_next tokens in
+(init_node PROG (p._token) [n]) 
 let compile_begin file = 
   let file_dat = read_file file in
   let t = List.rev (lex_all {src = file_dat; pos = 0; line = 1; col = 0} []) in
-  print_endline (read_file file); Printf.printf "%s\n" (string_of_token_list t)
+  print_endline (read_file file); Printf.printf "%s\n" (string_of_token_list t); Printf.printf "%s\n" (string_of_node (parse t))
 
 
 
